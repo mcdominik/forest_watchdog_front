62fb79fbcb29f82eb93b4730cc1bf4893beeed2a
mcdominik <dominik8881@wp.pl>
2023-01-19 17:32:33 +0100

    new url

--- script.js
+++ script.js
@@ -86,7 +86,9 @@ window.onload = () => {
 const uploadZone = document.getElementById('upload-button');
 
 let file = null;
-const baseUrl = 'http://localhost:8000';
+// const baseUrl = 'http://localhost:8000';
+const baseUrl = "https://forestapi.bieda.it";
+
 
 const getCurrentMode = async () => {
   const response = await fetch(`${baseUrl}/getMyDevice`);
